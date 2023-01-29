f7608b7440527e675ed9db8256ba638564914ec8
grisha <grisha.endorphone@gmail.com>
2023-01-29 11:58:02 +0200

    first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button";
+import ResultField from "./components/ResultField";
+
+const buttonsValue = [
+  ['C', '√', '%', '/'],
+  ['7', '8', '9', 'x'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '=']
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <ResultField />
+      <div className="buttons--area">
+        {buttonsValue.flat().map((button, i) => (
+          <Button value={button} key={i} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/ResultField.js
@@ -0,0 +1,7 @@
+const ResultField = () => {
+  return (
+    <div className="result--field">12432325</div>
+  );
+}
+
+export default ResultField;
